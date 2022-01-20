e43d968010254b62e5c7391d457347c5af9fe137
hojat <hojat72elect@gmail.com>
2022-01-19 21:53:09 -0500

    The width and height of the machine that runs the game are saved and accessed via 2 Constants all throughout the project.

---
+++ app/src/main/java/ca/sudbury/hojat/flappybird/Constants.java
@@ -0,0 +1,9 @@
+package ca.sudbury.hojat.flappybird;
+
+/**
+ * Shared constants throughout the project.
+ */
+public class Constants {
+    public s325tatic int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+}
--- app/src/main/java/ca/sudbury/hojat/flappybird/MainActivity.java
+++ app/src/main/java/ca/sudbury/hojat/flappybird/MainActivity.java
@@ -1,6 +1,7 @@
 package ca.sudbury.hojat.flappybird;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//hides the status bar
+
+        //Accessing the width and height of machine that is running the game.
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Constants.SCREEN_HEIGHT = dm.heightPixels;
+        Constants.SCREEN_WIDTH = dm.widthPixels;
+
         setContentView(R.layout.activity_main);
     }
 }
