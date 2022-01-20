e9818fa9037ea59c076c83c5c9e07881d0766ca0
hojat <hojat72elect@gmail.com>
2022-01-19 21:12:58 -0500

    Made the game window to look full screen.

--- app/src/main/java/ca/sudbury/hojat/flappybird/MainActivity.java
+++ app/src/main/java/ca/sudbury/hojat/flappybird/MainActivity.java
@@ -1,14 +1,16 @@
 package ca.sudbury.hojat.flappybird;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//hides the status bar
         setContentView(R.layout.activity_main);
     }
 }
