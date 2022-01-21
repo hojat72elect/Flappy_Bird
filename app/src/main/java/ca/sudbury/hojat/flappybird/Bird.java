b9109c4118b43fa38b3453c3992538e208d00297
hojat <hojat72elect@gmail.com>
2022-01-21 00:00:49 -0500

    Applied the Bitmap of the Bird, initialized it in GameView.java and drew it on the game screen.

--- app/src/main/java/ca/sudbury/hojat/flappybird/Bird.java
+++ app/src/main/java/ca/sudbury/hojat/flappybird/Bird.java
@@ -18,16 +18,34 @@ public class Bird extends BaseObject {
     public Bird() {
     }
 
-    //drawing the bird on the screen
+    //drawing the bird's Bitmap on the Canvas.
     public void draw(Canvas canvas) {
-
+        canvas.drawBitmap(this.getBm(), this.x, this.y, null);
     }
 
     public ArrayList<Bitmap> getArrBms() {
         return arrBms;
     }
 
+    /**
+     * The bitmaps that are set into this Bird object should be re-scaled according to the width and height of the bird object for each screen (remember that width and height of the Bird object are set according to the device's screen dimensions).
+     *
+     * @param arrBms
+     */
     public void setArrBms(ArrayList<Bitmap> arrBms) {
         this.arrBms = arrBms;
+        for (int i = 0; i < arrBms.size(); i++) {
+            this.arrBms.set(i, Bitmap.createScaledBitmap(this.arrBms.get(i), this.width, this.height, true));//rescaling a bitmap in android.
+        }
+    }
+
+    /**
+     * implementing Bitmap getter of its parent(only returns a single bitmap not an array of them).
+     *
+     * @return
+     */
+    @Override
+    public Bitmap getBm() {
+        return this.getArrBms().get(0);
     }
 }
--- app/src/main/java/ca/sudbury/hojat/flappybird/GameView.java
+++ app/src/main/java/ca/sudbury/hojat/flappybird/GameView.java
@@ -1,11 +1,15 @@
 package ca.sudbury.hojat.flappybird;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class GameView extends View {
     private Bird bird;
@@ -18,11 +22,29 @@ public class GameView extends View {
      */
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        //creating the bird object.
+        bird = new Bird();
+
+        //setting the width and height of the bird.
+        bird.setWidth(100 * Constants.SCREEN_WIDTH / 1080);
+        bird.setHeight(100 * Constants.SCREEN_HEIGHT / 1920);
+
+        //setting the coordinates of the bird at the start of the game.
+        bird.setX((float) 100 * Constants.SCREEN_WIDTH / 1080);
+        bird.setY((float) (Constants.SCREEN_HEIGHT / 2 - bird.getHeight() / 2));
+
+        //the 2 pics that show different states of bird will be loaded into an array of bitmaps and fed to Bird object.
+        ArrayList<Bitmap> arrBms = new ArrayList<>();
+        arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.bird1));
+        arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.bird2));
+        bird.setArrBms(arrBms);
+
 
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+        bird.draw(canvas);//initial drawing of the bird on screen.
     }
 }
